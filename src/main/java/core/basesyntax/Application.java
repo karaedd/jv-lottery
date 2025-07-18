e7927c067986313211a1d59b9e4bf5d9ffd1d9fc
kraievskyi-om <kraievskyi-om@ukrposhta.com>
2025-07-18 11:42:06 +0300

    Implement basic lottery logic: Ball class, Color enum, ColorSupplier, and Lottery generator

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import java.util.stream.IntStream;
+
 public class Application {
+    private static final int ZERO = 0;
+    private static final int THREE = 3;
+
     public static void main(String[] args) {
-        // create three balls using class Lottery and print information about them in console
+        Lottery lottery = new Lottery();
+        IntStream.range(ZERO, THREE).forEach(i -> System.out.println(lottery.getRandomBall()));
     }
 }
